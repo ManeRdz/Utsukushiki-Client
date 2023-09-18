56a94c4108f5b45cdd8f173f8402ee3fecc40137
ManeRdz <manuelpcrdz@gmail.com>
2023-09-18 10:53:48 -0600

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://manerdz.github.io/Utsukushiki-Client/",
+  base: "/Utsukushiki-Client/",
 });
